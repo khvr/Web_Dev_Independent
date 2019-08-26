fc4266cd317edf96fd71d3e9211eabd7ca44e5f3
Harsha-kalyanaraman <kalyanaraman.ha@husky.neu.edu>
2019-08-25 22:13:34 -0400

    Connecting Angular frontend to API

--- back-end/server.js
+++ back-end/server.js
@@ -13,6 +13,12 @@ app.listen(PORT,()=>{
 // Load middleware
 app.use(bodyParser.json());
 
+// CORS HEADERS MIDDLEWARE
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 /* ROUTE HANDLERS*/
 
 /*LIST ROUTES*/
